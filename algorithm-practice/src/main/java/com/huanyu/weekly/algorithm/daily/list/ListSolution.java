be1977ef541c6acf0846b9dd73ae4aac029233d4
chenjingyu <chenjingyu@videopls.com>
2020-03-24 09:30:18 +0800

    合并两个有序链表

--- algorithm-practice/src/main/java/com/huanyu/weekly/algorithm/daily/list/ListSolution.java
+++ algorithm-practice/src/main/java/com/huanyu/weekly/algorithm/daily/list/ListSolution.java
@@ -10,6 +10,94 @@ public class ListSolution {
         }
     }
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode dummyHead = new ListNode(0);
+        ListNode m = dummyHead;
+        while (p != null && q != null) {
+            if (p.val < q.val) {
+                ListNode temp = p.next;
+                p.next = null;
+                m.next = p;
+                p = temp;
+            } else {
+                ListNode temp = q.next;
+                q.next = null;
+                m.next = q;
+                q = temp;
+            }
+            m = m.next;
+        }
+
+        if (p != null) {
+            m.next = p;
+        }
+
+        if (q != null) {
+            m.next = q;
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode p = dummyHead;
+        ListNode q = dummyHead;
+        for (int i = 0; i < n+1; i++) {
+            if (q == null) {
+                break;
+            }
+            q = q.next;
+        }
+
+        while (q != null) {
+            p = p.next;
+            q = q.next;
+        }
+
+        p.next = p.next.next;
+        return dummyHead.next;
+    }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+
+        int w = 1;
+        ListNode h = head;
+        while (h.next != null) {
+            h = h.next;
+            w++;
+        }
+
+        if (w == 1 && n == 1) {
+            head = null;
+            return null;
+        }
+
+        int r = w - n;
+        if (r == 0) {
+          head.val = head.next.val;
+          head.next = head.next.next;
+          return head;
+        }
+        ListNode p = head;
+        for (int i = 1; i < r; i++) {
+            if (p == null) {
+                break;
+            }
+            p = p.next;
+        }
+
+        if (p != null && p.next != null) {
+            p.next = p.next.next;
+        }
+        return head;
+    }
+
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
@@ -29,8 +117,6 @@ public class ListSolution {
         }
         p.next = new ListNode(a);
     }
-
-
 
     public ListNode reverseList(ListNode head) {
         if (head == null) {
@@ -129,9 +215,12 @@ public class ListSolution {
         ListSolution listSolution = new ListSolution();
         listSolution.createList(1);
         listSolution.createList(2);
-        listSolution.print(listSolution.head);
-        ListNode head = listSolution.reverseList(listSolution.head);
-        System.out.println("Reverse List: ");
+        listSolution.createList(4);
+        ListSolution listSolution2 = new ListSolution();
+        listSolution2.createList(1);
+        listSolution2.createList(3);
+        listSolution2.createList(4);
+        ListNode head = listSolution.mergeTwoLists(listSolution.head, listSolution2.head);
         listSolution.print(head);
     }
 }
